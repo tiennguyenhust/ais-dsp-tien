580d63522822b3b328c3d506f92631e9f024cafb
Sherlock <tiennguyenhust@.com>
2021-05-08 22:50:23 +0200

    update

--- app/feature_engineering.py
+++ app/feature_engineering.py
@@ -1,20 +1,32 @@
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 import sklearn
+import pickle
+
+def save_one_encoder(dataframe: pd.DataFrame):
+    categorical_columns = list(dataframe.select_dtypes(include='object').columns)
+    one_hot_encoder = OneHotEncoder(handle_unknown="ignore")
+    one_hot_encoder.fit(dataframe[categorical_columns])
+    filename = '../data/one_encoder.sav'
+    pickle.dump(one_hot_encoder, open(filename, 'wb'))
+
+# def get_one_encoder(dataframe: pd.DataFrame) -> sklearn.preprocessing._encoders.OneHotEncoder:
+#     categorical_columns = list(dataframe.select_dtypes(include='object').columns)
+#     one_hot_encoder = OneHotEncoder(handle_unknown="ignore", sparse=False)
+#     one_hot_encoder.fit(dataframe[categorical_columns])
+#     return one_hot_encoder
+
+def get_one_encoder() -> sklearn.preprocessing._encoders.OneHotEncoder:
+    filename = '../data/one_encoder.sav'
+    return pickle.load(open(filename, 'rb'))
 
 def encode_categorical_data(dataframe: pd.DataFrame) -> pd.DataFrame:
     categorical_columns = list(dataframe.select_dtypes(include='object').columns)
-    one_hot_encoder = OneHotEncoder()
-    encoded_categorical_df = one_hot_encoder.fit_transform(dataframe[categorical_columns]).toarray()
+    one_hot_encoder = get_one_encoder()
+    encoded_categorical_df = one_hot_encoder.transform(dataframe[categorical_columns]).toarray()
     df = pd.DataFrame(encoded_categorical_df)
     df.index += 1
     return df
-
-def get_one_encoder(dataframe: pd.DataFrame) -> sklearn.preprocessing._encoders.OneHotEncoder:
-    categorical_columns = list(dataframe.select_dtypes(include='object').columns)
-    one_hot_encoder = OneHotEncoder()
-    one_hot_encoder.fit(dataframe[categorical_columns])
-    return one_hot_encoder
 
 def feature_engineer(dataframe: pd.DataFrame) -> pd.DataFrame:
     encoded_categorical_df = encode_categorical_data(dataframe)
--- app/output.py
+++ app/output.py
@@ -1,5 +1,5 @@
 def make_output(X_test_id, y_pred):
-    with open('submission.csv', 'w') as writer:
+    with open('../data/submission.csv', 'w') as writer:
         n = len(y_pred)
         
         writer.write('Id,SalePrice')
